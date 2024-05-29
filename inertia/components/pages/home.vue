0cded76e9513899601502308f0d24f4e94a0e7f0
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-05-29 11:30:12 +0700

    chore: added fully functioning pagination on home results

--- inertia/components/pages/home.vue
+++ inertia/components/pages/home.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { InferPageProps } from '@adonisjs/inertia/types'
 import type PropertiesController from '#controllers/properties_controller'
-import { toRefs } from 'vue'
+import { ref, toRefs, watch } from 'vue'
 import Property from '../molecules/Property.vue'
+import Paginator from 'primevue/paginator'
+import { router } from '@inertiajs/vue3'
 
 const props = defineProps<{
   properties: InferPageProps<
@@ -11,14 +13,19 @@ const props = defineProps<{
   >['properties']
 }>()
 const { properties } = toRefs(props)
+
+const currentPage = ref((properties.value.page.current - 1) * properties.value.page.perPage)
+
+watch(currentPage, async (newItemNumbers) => {
+  const pageNumber = newItemNumbers / properties.value.page.perPage + 1
+  router.get('/homes', { page: pageNumber }, { only: ['properties'] })
+})
 </script>
 
 <template>
-  <div class="container">
+  <div class="container mx-auto">
     <ul id="propertyDisplay">
-      <li v-for="property in properties.data" :key="property.id">
-        <Property :property="property" />
-      </li>
+      <Property :property="property" v-for="property in properties.data" :key="property.id" />
     </ul>
 
     <span>
@@ -26,4 +33,26 @@ const { properties } = toRefs(props)
       <a href="https://docs.adonisjs.com/guides/inertia">AdonisJS documentation</a>.
     </span>
   </div>
+  <Paginator
+    :template="{
+      '640px': 'PrevPageLink CurrentPageReport NextPageLink',
+      '960px': 'FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink',
+      '1300px': 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink',
+      'default': 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink JumpToPageInput',
+    }"
+    :rows="properties.page.perPage"
+    :totalRecords="properties.page.total"
+    v-model:first="currentPage"
+  >
+  </Paginator>
 </template>
+
+<style scoped>
+#propertyDisplay {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 1rem;
+  width: 80%;
+  min-width: 300px;
+}
+</style>
